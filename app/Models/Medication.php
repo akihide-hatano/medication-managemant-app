feec89b7aa31f0d2113ff2ad3cd6d6a25cdd559a
akihide-hatano <a.h.19841202@gmail.com>
2025-08-14 07:21:58 +0900

    Medication.phpを作成しました。

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Medication extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'medications';
+
+    protected $primaryKey = 'medication_id';
+
+    protected $fillable = [
+        'medication_name',
+        'dosage',
+        'notes',
+        'effects',
+        'side_effects',
+    ];
+    public function recordMedications()
+    {
+        return $this->hasMany(RecordMedication::class, 'medication_id', 'medication_id');
+    }
 }
