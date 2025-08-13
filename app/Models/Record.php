a955c443a450c43ffa25482eb3a4b4c0d150b13a
akihide-hatano <a.h.19841202@gmail.com>
2025-08-13 22:31:11 +0900

    Record.phpの修正をしました。

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Record extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'records';
+    protected $primaryKey = 'record_id';
+
+    protected $fillable = [
+    'user_id',
+    'timing_id',
+    ];
+
+        public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    public function timingTag()
+    {
+        return $this->belongsTo(TimingTag::class, 'timing_id', 'timing_id');
+    }
+
+    public function recordMedications()
+    {
+        return $this->hasMany(RecordMedication::class, 'record_id', 'record_id');
+    }
 }
