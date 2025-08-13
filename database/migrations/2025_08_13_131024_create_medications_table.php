164b5af3dc9dbda43a9469e680bd01e6fc93ad20
akihide-hatano <a.h.19841202@gmail.com>
2025-08-14 07:38:00 +0900

    2025_08_13_131024_create_medications_tableを作成しました。

--- database/migrations/2025_08_13_131024_create_medications_table.php
+++ database/migrations/2025_08_13_131024_create_medications_table.php
@@ -12,7 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('medications', function (Blueprint $table) {
-            $table->id();
+            $table->id('medication_id');
+            $table->string('medication_name');
+            $table->string('dosaga');
+            $table->text('effects');
+            $table->text('side_effects');
+            $table->timestamps();
+
             $table->timestamps();
         });
     }
