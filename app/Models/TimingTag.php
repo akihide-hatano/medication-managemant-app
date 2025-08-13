4d684e97a671ab22c5971e4864ab58528c99ced8
akihide-hatano <a.h.19841202@gmail.com>
2025-08-13 22:38:02 +0900

    TimingTagを修正しました。

--- app/Models/TimingTag.php
+++ app/Models/TimingTag.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TimingTag extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'timing_tags';
+    protected $primaryKey = 'timing_id';
+
+    protected $fillable = [
+        'timing_name',
+        'base_time',
+    ];
+
+    protected $casts = [
+        'base_time' => 'datetime:H:i:s',
+    ];
+
+    public function records()
+    {
+        return $this->hasMany(Record::class, 'timing_id', 'timing_id');
+    }
 }
